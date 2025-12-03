ea621bd55e9e13d7641eebedda3392d590ab2e32
Aleksandr Chumachenko <aleksandr.chumachenko@4k.com.ua>
2025-12-03 09:02:32 +0200

    feat: add day 2

--- cmd/day02/main.go
+++ cmd/day02/main.go
@@ -1,28 +1,64 @@
 package main
 
 import (
-	"bufio"
+	"adventofcode_2025/internal"
 	"fmt"
-	"os"
-	"sort"
+	"log"
+	"strconv"
+	"strings"
 )
 
+type Range struct {
+	start int
+	end   int
+}
+
+func parseRanges(text string) []Range {
+	text = strings.TrimSpace(text)
+	parts := strings.Split(text, ",")
+	result := make([]Range, 0, len(parts))
+
+	for _, p := range parts {
+		edges := strings.Split(p, "-")
+		start, _ := strconv.Atoi(edges[0])
+		end, _ := strconv.Atoi(edges[1])
+
+		result = append(result, Range{
+			start: start,
+			end:   end,
+		})
+	}
+
+	return result
+}
+
+func hasEqualHalves(n int) bool {
+	numberStr := strconv.Itoa(n)
+	if len(numberStr)%2 != 0 {
+		return false
+	}
+
+	mid := len(numberStr) / 2
+	return numberStr[:mid] == numberStr[mid:]
+}
+
 func main() {
-	in := bufio.NewReader(os.Stdin)
-	out := bufio.NewWriter(os.Stdout)
-	defer out.Flush()
+	countInvalid := 0
 
-	var n int
-	fmt.Fscan(in, &n)
-
-	arr := make([]int, n)
-	for i := 0; i < n; i++ {
-		fmt.Fscan(in, &arr[i])
+	text, err := internal.ReadText("cmd/day02/input.txt")
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	sort.Ints(arr)
+	ranges := parseRanges(text)
 
-	for _, v := range arr {
-		fmt.Fprint(out, v, " ")
+	for _, r := range ranges {
+		for i := r.start; i <= r.end; i++ {
+			if hasEqualHalves(i) {
+				countInvalid += i
+			}
+		}
 	}
+
+	fmt.Println("Final value:", countInvalid)
 }
--- internal/io.go
+++ internal/io.go
@@ -21,3 +21,11 @@ func ReadLines(path string) ([]string, error) {
 
 	return result, scanner.Err()
 }
+
+func ReadText(path string) (string, error) {
+	b, err := os.ReadFile(path)
+	if err != nil {
+		return "", err
+	}
+	return string(b), nil
+}
